a12c4685fcc7ffae2465cd69a3d41ea543e872da
David L. Inman <davidlevere@gmail.com>
2022-02-28 18:14:24 -0500

    inc prog

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+
+use std::io;
+
+fn read_line() -> io::Result<String> {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s)?;
+    Ok(s.trim_end().to_string())
+}
+
+enum Guess { 
+    Correct(char),
+    Present(char),
+    Wrong,
+}
+
+fn format_guess(guess : Vec<Guess>) -> String {
+    fn x(g : Guess) -> String {
+        match g {
+            Guess::Correct(c) => format!("|{c}|"),
+            Guess::Present(c) => format!("~{c}~"),
+            Guess::Wrong => " _ ".to_string(),
+        }
+    }
+
+    guess.into_iter().map(x).collect()
+}
+
+fn evaluate_guess(user_guess : &str, right_answer : &str) -> Vec<Guess> {
+    vec![]
+}
+
+fn main() -> io::Result<()> {
+    loop {
+        let x = read_line()?;
+
+        println!("{}", x.len());
+
+    }
+    Ok(())
 }
