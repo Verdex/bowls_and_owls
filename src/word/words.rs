26f42738654184c6137d7887b883fe75b9488aa9
David L. Inman <davidlevere@gmail.com>
2022-03-02 21:45:17 -0500

    fix issue with word import

--- src/word/words.rs
+++ src/word/words.rs
@@ -6,10 +6,11 @@ static WORDS : &'static str = include_str!("words.txt");
 pub fn get_words() -> HashMap<usize, Vec<&'static str>> {
     let mut r : HashMap<usize, Vec<&'static str>> = HashMap::new();
     for word in WORDS.split('\n') {
-        let l = word.len();
-        match r.get_mut(&l) {
-            Some(v) => v.push(word),
-            None => { r.insert(l, vec![word]); },
+        let w = word.trim_end();
+        let length = w.len();
+        match r.get_mut(&length) {
+            Some(v) => v.push(w),
+            None => { r.insert(length, vec![w]); },
         }
     }
     r
