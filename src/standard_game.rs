2e453de097dfc2e237b98d55b7466e2219b3af6b
David L. Inman <davidlevere@gmail.com>
2022-03-04 00:11:24 -0500

    use utility evaluate_guess; remove redundant tests

--- src/standard_game.rs
+++ src/standard_game.rs
@@ -1,5 +1,5 @@
 
-use crate::game::{Guess, Game};
+use crate::game::{self, Guess, Game};
 use crate::word::Standard;
 
 pub struct G<'a> {
@@ -20,27 +20,7 @@ impl<'a> G<'a> {
 
 impl<'a> Game for G<'a> {
     fn evaluate_guess(&mut self, user_guess : &str) -> Vec<Guess> {
-        let a = self.current.chars().collect::<Vec<char>>();
-        let u = user_guess.chars().collect::<Vec<char>>();
-        let mut r = a.iter().map(|x| Guess::Wrong(*x)).collect::<Vec<Guess>>();
-
-        let mut present = vec![];
-        for i in 0..a.len() {
-            if a[i] == u[i] {
-                r[i] = Guess::Correct(u[i])
-            }
-            else {
-                present.push(a[i]);
-            }
-        }
-
-        for i in 0..a.len() {
-            if a[i] != u[i] && present.contains(&u[i]) {
-                r[i] = Guess::Present(u[i])
-            }
-        }
-
-        r
+        game::evaluate_guess(self.current, user_guess)
     }
 
     fn letter_count(&self) -> usize {
@@ -57,114 +37,5 @@ impl<'a> Game for G<'a> {
 
         self.current = next; 
         true
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn should_evaluate_wrong_guess() {
-        let mut g = G { length: 4, current: "RRBB", words: Standard::new()};
-        let guess = g.evaluate_guess("XXXX");
-
-        assert!( matches!(&guess[..], [Guess::Wrong(_), Guess::Wrong(_), Guess::Wrong(_), Guess::Wrong(_)]) );
-    }
-
-    #[test]
-    fn should_evaluate_one_correct() {
-        let mut g = G { length: 4, current: "XAAA", words: Standard::new()};
-        let guess = g.evaluate_guess("XBBB");
-
-        assert!( matches!(&guess[..], [Guess::Correct(_), Guess::Wrong(_), Guess::Wrong(_), Guess::Wrong(_)]) );
-    }
-
-    #[test]
-    fn should_evaluate_two_correct() {
-        let mut g = G { length: 4, current: "XAAY", words: Standard::new()};
-        let guess = g.evaluate_guess("XBBY");
-
-        assert!( matches!(&guess[..], [Guess::Correct(_), Guess::Wrong(_), Guess::Wrong(_), Guess::Correct(_)]) );
-    }
-
-    #[test]
-    fn should_evaluate_three_correct() {
-        let mut g = G { length: 4, current: "XAZY", words: Standard::new()};
-        let guess = g.evaluate_guess("XBZY");
-
-        assert!( matches!(&guess[..], [Guess::Correct(_), Guess::Wrong(_), Guess::Correct(_), Guess::Correct(_)]) );
-    }
-
-    #[test]
-    fn should_evaluate_four_correct() {
-        let mut g = G { length: 4, current: "XWZY", words: Standard::new()};
-        let guess = g.evaluate_guess("XWZY");
-
-        assert!( matches!(&guess[..], [Guess::Correct(_), Guess::Correct(_), Guess::Correct(_), Guess::Correct(_)]) );
-    }
-
-    #[test]
-    fn should_evaluate_one_present() {
-        let mut g = G { length: 4, current: "ZAZZ", words: Standard::new()};
-        let guess = g.evaluate_guess("XXXA");
-
-        assert!( matches!(&guess[..], [Guess::Wrong(_), Guess::Wrong(_), Guess::Wrong(_), Guess::Present(_)]) );
-    }
-
-    #[test]
-    fn should_evaluate_two_present() {
-        let mut g = G { length: 4, current: "ZAZB", words: Standard::new()};
-        let guess = g.evaluate_guess("BXXA");
-
-        assert!( matches!(&guess[..], [Guess::Present(_), Guess::Wrong(_), Guess::Wrong(_), Guess::Present(_)]) );
-    }
-
-    #[test]
-    fn should_evaluate_three_present() {
-        let mut g = G { length: 4, current: "CAZB", words: Standard::new()};
-        let guess = g.evaluate_guess("BCXA");
-
-        assert!( matches!(&guess[..], [Guess::Present(_), Guess::Present(_), Guess::Wrong(_), Guess::Present(_)]) );
-    }
-
-    #[test]
-    fn should_evaluate_four_present() {
-        let mut g = G { length: 4, current: "CADB", words: Standard::new()};
-        let guess = g.evaluate_guess("BCAD");
-
-        assert!( matches!(&guess[..], [Guess::Present(_), Guess::Present(_), Guess::Present(_), Guess::Present(_)]) );
-    }
-
-    #[test]
-    fn should_evaluate_with_no_present_for_extra() {
-        let mut g = G { length: 4, current: "RRBB", words: Standard::new()};
-        let guess = g.evaluate_guess("RRRX");
-
-        assert!( matches!(&guess[..], [Guess::Correct(_), Guess::Correct(_), Guess::Wrong(_), Guess::Wrong(_)]) );
-    }
-
-    #[test]
-    fn should_evaluate_with_present_for_extra() {
-        let mut g = G { length: 4, current: "RRBR", words: Standard::new()};
-        let guess = g.evaluate_guess("RRRX");
-
-        assert!( matches!(&guess[..], [Guess::Correct(_), Guess::Correct(_), Guess::Present(_), Guess::Wrong(_)]) );
-    }
-
-    #[test]
-    fn should_evaluate_with_present_and_correct() {
-        let mut g = G { length: 4, current: "ABDB", words: Standard::new()};
-        let guess = g.evaluate_guess("ABBX");
-
-        assert!( matches!(&guess[..], [Guess::Correct(_), Guess::Correct(_), Guess::Present(_), Guess::Wrong(_)]) );
-    }
-
-    #[test]
-    fn should_evaluate_with_correct_but_no_present_for_consumed_correct() {
-        let mut g = G { length: 4, current: "RRBB", words: Standard::new()};
-        let guess = g.evaluate_guess("RRRB");
-
-        assert!( matches!(&guess[..], [Guess::Correct(_), Guess::Correct(_), Guess::Wrong(_), Guess::Correct(_)]) );
     }
 }
