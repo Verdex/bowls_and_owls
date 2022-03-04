c4eae3e183d9ee69169d425036d8143577427a60
David L. Inman <davidlevere@gmail.com>
2022-03-04 00:00:55 -0500

    there is now only one static hash map of words even if you initialize more than one standard

--- src/word/dictionary.rs
+++ src/word/dictionary.rs
@@ -1,23 +1,48 @@
 
 use std::collections::HashMap;
 use rand::prelude::*;
-use super::words;
+
+static WORDS : &'static str = include_str!("words.txt");
+static mut HASH : Option<HashMap<usize, Vec<&'static str>>> = None; 
+
+unsafe fn raw_get_word(length : usize, rng : &mut ThreadRng) -> Option<&'static str> {
+
+    fn g(h : &HashMap<usize, Vec<&'static str>>, length : usize, rng : &mut ThreadRng) -> Option<&'static str> {
+        let words = h.get(&length)?;
+        let index = rng.gen_range(0..words.len());
+        Some(words[index])
+    }
+
+    match &HASH {
+        None => {
+            let mut r : HashMap<usize, Vec<&'static str>> = HashMap::new();
+            for word in WORDS.split('\n') {
+                let w = word.trim_end();
+                let length = w.len();
+                match r.get_mut(&length) {
+                    Some(v) => v.push(w),
+                    None => { r.insert(length, vec![w]); },
+                }
+            }
+            let ret = g(&r, length, rng);
+            HASH = Some(r);
+            ret
+        },
+        Some(h) => g(&h, length, rng),
+    }
+}
 
 pub struct Standard {
     rng : ThreadRng,
-    words : HashMap<usize, Vec<&'static str>>,
 }
 
 impl Standard {
     pub fn new() -> Self {
         Standard { rng : thread_rng()
-                 , words : words::get_words()
                  }
     }
 
     pub fn get_word(&mut self, length : usize) -> Option<&'static str> {
-        let words = self.words.get(&length)?;
-        let index = self.rng.gen_range(0..words.len());
-        Some(words[index])
+        unsafe { raw_get_word(length, &mut self.rng) }
     }
 }
