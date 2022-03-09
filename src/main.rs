3c915fb0f5394ff36d6398b069215e27d2aa4ee9
David L. Inman <davidlevere@gmail.com>
2022-03-08 22:51:06 -0500

    resolve todos

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod word;
 
 mod standard_game;
 
+use std::collections::HashSet;
 use std::io::{self, Error, ErrorKind};
 use crate::game::{Game, Guess};
 use crate::word::Standard;
@@ -14,7 +15,6 @@ fn read_line() -> io::Result<String> {
     Ok(s.trim_end().to_string())
 }
 
-
 fn main() -> io::Result<()> {
     let mut words = Standard::new();
     let mut game = standard_game::G::new(words.get_word(4).ok_or(Error::new(ErrorKind::Other, "failed to get word"))?);
@@ -22,11 +22,12 @@ fn main() -> io::Result<()> {
     loop {
         let letter_count = game.letter_count();
         println!("word size: {}", letter_count);
+        let mut wrong_letters = HashSet::new();
         loop {
             let mut line = read_line()?;
             let mut input = line.trim_end();
             while input.len() != letter_count {
-                // TODO tell the user that their letter count was wrong
+                println!("expected word size: {}", letter_count);
                 line = read_line()?;
                 input = line.trim_end();
             }
@@ -34,14 +35,23 @@ fn main() -> io::Result<()> {
             let guess = game.evaluate_guess(input);
             let guess_output = game::format_guess(&guess);
 
+            for g in guess.iter() {
+                match g { 
+                    Guess::Wrong(letter) => { wrong_letters.insert(*letter); },
+                    _ => { },
+                }
+            }
+
+            let wrong_letter_output = wrong_letters.iter().map(|c| c.to_string()).collect::<Vec<_>>().join(" ");
+
             println!("\t\t{}", guess_output);
-            // TODO display all used letters
+
+            println!("\n{}", wrong_letter_output);
 
             if guess.iter().all(|x| matches!(x, Guess::Correct(_))) {
                 break;
             }
         }
-        // TODO tell the user that they're on a new word
         game.next_word();
     }
     Ok(())
